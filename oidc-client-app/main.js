c966d9a9b2a2505fa805627937dd02d2b0f98422
Kurt Googler Kanaskie <kurtkanaskie@google.com>
2017-04-13 17:00:56 -0400

    Still Adding Customers via Routing Example

--- oidc-client-app/main.js
+++ oidc-client-app/main.js
@@ -15,20 +15,19 @@ mainApp.config(function($routeProvider) {
 		});
 });
 
-mainApp.controller('HomeController', function($scope) {
-	$scope.message = "Click to view customers.";
+mainApp.controller('HomeController', function($scope, $http) {
+	$scope.message = "Here, Click to view customers.";
 });
 
 mainApp.controller('CustomerController', function($scope, $http) {
-    $scope.message = "Here";
 	$http({
 		method : "GET",
 		url : "https://apibaas-trial.apigee.net/kurtkanaskie/sandbox/customers"
 	}).then(function mySucces(response) {
-      $scope.message = "OK";
+      $scope.status = "OK";
       $scope.customers = response.data.entities;
     }, function myError(response) {
-      $scope.message = "Uh oh";
+      $scope.status = "Uh oh";
       $scope.customers = response.status;
 	});
 
