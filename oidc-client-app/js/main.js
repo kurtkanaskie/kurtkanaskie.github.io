bf5711449365c467e0ea41e064ac6df46b79e101
Kurt Googler Kanaskie <kurtkanaskie@google.com>
2017-04-14 12:49:27 -0400

    Still Adding in authorize for implicit flow

--- oidc-client-app/js/main.js
+++ oidc-client-app/js/main.js
@@ -27,7 +27,7 @@ mainApp.controller('HomeController', function($scope, $http) {
 	$scope.message = "Click to authorize then view customers.";
     var date = new Date();
     $scope.date = date;
-	$scope.callback = "https://tmobileh-sb05.apigee.net/oidc-core/oauth2/authorize?client_id=AO7wf24CFswJeX6UmaKdbRcJ1uhMJaoh&redirect_uri=https://kurtkanaskie.github.io/oidc-client-app/index.html#/callback.html&response_type=token+id_token&state=A&scope=openid+profile&nonce=" + date;
+	$scope.callback = "https://tmobileh-sb05.apigee.net/oidc-core/oauth2/authorize?client_id=AO7wf24CFswJeX6UmaKdbRcJ1uhMJaoh&redirect_uri=https://kurtkanaskie.github.io/oidc-client-app/callback.html&response_type=token+id_token&state=A&scope=openid+profile&nonce=" + date;
 });
 
 mainApp.controller('AuthorizeController', function($scope, $http) {
