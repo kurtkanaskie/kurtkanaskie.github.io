c0a46ffa92c8430a2f9f0e83e063583c7c899ba0
Kurt Googler Kanaskie <kurtkanaskie@google.com>
2017-04-14 10:56:54 -0400

    Adding in authorize for implicit flow

---
+++ oidc-client-app/js/main.js
@@ -0,0 +1,53 @@
+var mainApp = angular.module("mainApp", ['ngRoute']);
+
+mainApp.config(function($routeProvider) {
+	$routeProvider
+		.when('/home', {
+			templateUrl: 'home.html',
+			controller: 'HomeController'
+		})
+		.when('/authorize', {
+			templateUrl: 'authorize.html',
+			controller: 'AuthorizeController'
+		})
+		.when('/callback', {
+			templateUrl: 'callback.html',
+			controller: 'CallbackController'
+		})
+		.when('/customers', {
+			templateUrl: 'customers.html',
+			controller: 'CustomersController'
+		})
+		.otherwise({
+			redirectTo: '/home'
+		});
+});
+
+mainApp.controller('HomeController', function($scope, $http) {
+	$scope.message = "Click to authorize then view customers.";
+});
+
+mainApp.controller('AuthorizeController', function($scope, $http) {
+	$scope.message = "Complete form, using id_token for implicit flow, code for code flow.";
+	$http({
+		method : "GET",
+		url : "https://tmobileh-sb05.apigee.net/oidc-core/oauth2/authorize?client_id=AO7wf24CFswJeX6UmaKdbRcJ1uhMJaoh&redirect_uri=https://kurtkanaskie.github.io/oidc-client-app/index.html#/callback.html&response_type=token+id_token&state=A&scope=openid+profile&nonce="
+	});
+});
+
+mainApp.controller('CallbackController', function($scope, $http) {
+	$scope.message = "Callback Handler";
+});
+
+mainApp.controller('CustomersController', function($scope, $http) {
+	$http({
+		method : "GET",
+		url : "https://apibaas-trial.apigee.net/kurtkanaskie/sandbox/customers"
+	}).then(function mySucces(response) {
+      $scope.status = "OK";
+      $scope.customers = response.data.entities;
+    }, function myError(response) {
+      $scope.status = "Uh oh";
+      $scope.customers = response.status;
+	});
+});
