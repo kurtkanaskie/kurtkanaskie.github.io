3b1fdebf3d7d1b3fbef2edcc2c23698ba53b6c7d
Kurt Googler Kanaskie <kurtkanaskie@google.com>
2017-04-17 13:59:32 -0400

    Plan C Raboy

--- oidc-implicit/app.js
+++ oidc-implicit/app.js
@@ -17,7 +17,7 @@ example.config(function($stateProvider, $urlRouterProvider) {
  
 example.controller("LoginController", function($scope) {
  
-	var url = "https://tmobileh-sb05.apigee.net/oidc-core/oauth2/authorize?client_id=AO7wf24CFswJeX6UmaKdbRcJ1uhMJaoh&redirect_uri=https://kurtkanaskie.github.io/oidc-implicit/outh_callback.html&response_type=token+id_token&state=A&scope=openid+profile&nonce=" + Date.now();
+	var url = "https://tmobileh-sb05.apigee.net/oidc-core/oauth2/authorize?client_id=AO7wf24CFswJeX6UmaKdbRcJ1uhMJaoh&redirect_uri=https://kurtkanaskie.github.io/oidc-implicit/oauth_callback.html&response_type=token+id_token&state=A&scope=openid+profile&nonce=" + Date.now();
     $scope.login = function() {
         window.location.href = url;
     }
