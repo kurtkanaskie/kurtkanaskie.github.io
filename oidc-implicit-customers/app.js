d6877a904322bc22d3c8582018f4c3e15d2a643c
Kurt Googler Kanaskie <kurtkanaskie@google.com>
2017-04-19 10:12:08 -0400

    Sign out cleanup

--- oidc-implicit-customers/app.js
+++ oidc-implicit-customers/app.js
@@ -16,61 +16,61 @@ app.config(function($stateProvider, $urlRouterProvider) {
 });
  
 app.controller("HomeController", function($scope, $state, $window) {
-	console.log( "HomeController" );
  
-	var url = "https://tmobileh-sb05.apigee.net/oidc-core/oauth2/authorize?client_id=AO7wf24CFswJeX6UmaKdbRcJ1uhMJaoh&redirect_uri=https://kurtkanaskie.github.io/oidc-implicit-customers/callback.html&response_type=token+id_token&state=A&scope=openid+profile&nonce=" + Date.now();
-	$scope.authorize = url;
+    var url = "https://tmobileh-sb05.apigee.net/oidc-core/oauth2/authorize" 
+		+ "?client_id=AO7wf24CFswJeX6UmaKdbRcJ1uhMJaoh"
+		+ "&redirect_uri=https://kurtkanaskie.github.io/oidc-implicit-customers/callback.html"
+		+ "&response_type=token+id_token"
+		+ "&state=A"
+		+ "&scope=openid+profile&nonce=" + Date.now();
+
+	// This is used if we want to use a simple href instead of button
+    $scope.authorize = url;
 
     $scope.login = function() {
-		console.log( "log in" );
         $window.location.href = url;
     }
     $scope.logout = function() {
-		$window.localStorage.setItem("signedIn", "not ");
-		$window.localStorage.setItem("oidc", "");
-		$state.reload();
-		console.log( "log out" );
+        $window.localStorage.setItem("signedIn", "not ");
+        $window.localStorage.setItem("oidc", "");
+        $state.reload();
     }
 
-	var signedIn = $window.localStorage.getItem("signedIn");
-	console.log( "signedIn is: =" + signedIn + "=" );
-	if( signedIn === "" ) {
-		$window.localStorage.setItem("signedIn", "not ");
-		console.log( "signedIn: " + signedIn );
-	}
+    var signedIn = $window.localStorage.getItem("signedIn");
+    if( signedIn === "" ) {
+        $window.localStorage.setItem("signedIn", "not ");
+    }
 
-	$scope.signedIn = $window.localStorage.getItem("signedIn");
-	console.log( "Signed In: " + $scope.signedIn );
+    $scope.signedIn = $window.localStorage.getItem("signedIn");
  
 });
 
 app.controller("CustomersController", function($scope, $http, $window) {
  
-	var oidc = $window.localStorage.getItem("oidc");
-	console.log( "CustomersController oidc =" + oidc + "=" );
-	if( oidc === "" ) {
-		console.log( "not log in" );
-		  $scope.status = 401;
-		  $scope.message = "You are not logged in";
-	} else {
+    var oidc = $window.localStorage.getItem("oidc");
+    // local storage can only hold strings, don't use null
+    if( oidc === "" ) {
+          $scope.status = 401;
+          $scope.message = "You are not logged in";
+    } else {
 
-    	var token = JSON.parse($window.localStorage.getItem("oidc")).oauth.access_token;
+        var token = JSON.parse($window.localStorage.getItem("oidc")).oauth.access_token;
 
-		$http({
-			headers: {"Authorization":"Bearer " + token},
-			method : "GET",
-			url : "https://tmobileh-sb05.apigee.net/atwork/v5/customers"
-		}).then(function (response) {
-			console.log( "Customers OK: " + response.status + JSON.stringify(response.data) );
-		  $scope.status = response.status;
-		  $scope.message = "OK";
-		  $scope.customers = response.data.entities;
-		}, function (response) {
-			console.log( "Customers ERROR: " + response.status + JSON.stringify(response.data) );
-		  $scope.status = response.status;
-		  $scope.message = JSON.stringify(response.data);
-		  $scope.customers = [];
-		});
-	}
+        $http({
+            headers: {"Authorization":"Bearer " + token},
+            method : "GET",
+            url : "https://tmobileh-sb05.apigee.net/atwork/v5/customers"
+        }).then(function (response) {
+          // console.log( "Customers OK: " + response.status + JSON.stringify(response.data) );
+          $scope.status = response.status;
+          $scope.message = "OK";
+          $scope.customers = response.data.entities;
+        }, function (response) {
+          // console.log( "Customers ERROR: " + response.status + JSON.stringify(response.data) );
+          $scope.status = response.status;
+          $scope.message = JSON.stringify(response.data);
+          $scope.customers = [];
+        });
+    }
  
 });
