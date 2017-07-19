05d39fbca801bb7abc66e67e62c24ea4592fd2f4
Kurt Googler Kanaskie <kurtkanaskie@google.com>
2017-07-19 12:14:22 -0500

    First cut at oidc-client-token in wec-nonprod-dev

---
+++ oidc-client-token/app.js
@@ -0,0 +1,76 @@
+var app = angular.module("app", ['ui.router']);
+ 
+app.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: 'home.html',
+            controller: 'HomeController'
+        })
+        .state('customers', {
+            url: '/customers',
+            templateUrl: 'customers.html',
+            controller: 'CustomersController'
+        });
+    $urlRouterProvider.otherwise('/home');
+});
+ 
+app.controller("HomeController", function($scope, $state, $window) {
+ 
+    var url = $window.location.href;
+    var redirect = url.replace("index.html#/home","callback.html");
+	// This is the OIDC-core App
+    var authorize = "https://wec-nonprod-dev.apigee.net/oidc-core/oauth2/authorize" 
+		+ "?client_id=AO7wf24CFswJeX6UmaKdbRcJ1uhMJaoh"
+		+ "&redirect_uri=" + redirect
+		+ "&response_type=token+id_token"
+		+ "&state=A"
+		+ "&scope=openid+profile&nonce=" + Date.now();
+
+	// console.log( "URL: " + url + " REDIRECT: " + redirect + " AUTHORIZE: " + authorize );
+    $scope.login = function() {
+        $window.location.href = authorize;
+    }
+    $scope.logout = function() {
+        $window.localStorage.setItem("oidc", "");
+        $state.reload();
+    }
+
+    var oidc = $window.localStorage.getItem("oidc");
+    if( oidc === null || oidc === "" ) {
+        $scope.inOrOut = "out";
+    } else {
+		$scope.inOrOut = "in";
+	}
+ 
+});
+
+app.controller("CustomersController", function($scope, $http, $window) {
+ 
+    var oidc = $window.localStorage.getItem("oidc");
+    // local storage can only hold strings, if not set "null"
+    if( oidc === null || oidc === "" ) {
+          $scope.status = 401;
+          $scope.message = "You are not logged in";
+    } else {
+
+        var token = JSON.parse($window.localStorage.getItem("oidc")).oauth.access_token;
+
+        $http({
+            headers: {"Authorization":"Bearer " + token},
+            method : "GET",
+            url : "https://wec-nonprod-dev.apigee.net/atwork/v5/customers"
+        }).then(function successCallback(response) {
+          // console.log( "Customers OK: " + response.status + JSON.stringify(response.data) );
+          $scope.status = response.status;
+          $scope.message = "OK";
+          $scope.customers = response.data.entities;
+        }, function errorCallback(response) {
+          // console.log( "Customers ERROR: " + response.status + " - " + response.statusText + " - " + JSON.stringify(response.data) );
+          $scope.status = response.status;
+          $scope.message = response.statusText
+          $scope.customers = [];
+        });
+    }
+ 
+});
